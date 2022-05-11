c1d6e33448c9aee3b9afe1c29eb9392222dd928f
f-fagua <felipe.fagua@unity3d.com>
2022-05-11 10:53:34 -0500

    first version of the asset loader

--- Assets/Scripts/AssetLoader.cs
+++ Assets/Scripts/AssetLoader.cs
@@ -1,18 +1,64 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.AddressableAssets.ResourceLocators;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.UI;
 
 public class AssetLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private AssetReference[] m_AssetsToLoad;
+
+    private Sprite[] m_Sprites;
+
+    private Image m_Image;
+    
+    private int m_CurrentTexturesLoaded = -1;
+    
+    private Text m_DebugText;
+
+    private float m_LoadingCooldown = 1.0f;
+
+    private void Start()
     {
+        Caching.ClearCache();
+        Addressables.InitializeAsync().Completed += InitComplete;
+    }
+
+    private void InitComplete(AsyncOperationHandle<IResourceLocator> initOp)
+    {
+        if (initOp.Status == AsyncOperationStatus.Failed)
+        {
+            m_DebugText.color = Color.red;
+            m_DebugText.text = initOp.OperationException.Message + "\n";
+            m_DebugText.text += initOp.OperationException.StackTrace + "\n";
+        }
+        else
+            LoadNextTexture();
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LoadNextTexture()
     {
-        
+        if (++m_CurrentTexturesLoaded < m_AssetsToLoad.Length)
+            m_AssetsToLoad[m_CurrentTexturesLoaded].LoadAssetAsync<Sprite>().Completed += OnCompleted;
+    }
+
+    private void OnCompleted(AsyncOperationHandle<Sprite> loadingOp)
+    {
+        if (loadingOp.Status == AsyncOperationStatus.Failed)
+        {
+            m_DebugText.color = Color.red;
+            m_DebugText.text += $"Error loading texture number {m_CurrentTexturesLoaded}.\n";
+            m_DebugText.text += loadingOp.OperationException.Message + "\n";
+            m_DebugText.text += loadingOp.OperationException.StackTrace + "\n";
+        }
+        else
+        {
+            m_Sprites[m_CurrentTexturesLoaded] = loadingOp.Result;
+            m_Image.sprite = m_Sprites[m_CurrentTexturesLoaded];
+        }
+
+        Invoke("LoadNextTexture", m_LoadingCooldown);
     }
 }
